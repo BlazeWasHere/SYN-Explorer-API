b33d282376141677e9a37632aa1517eda7cca47b
BlazeWasHere <blaze@dorime.org>
2021-12-20 11:10:45 +0000

    raise errors more cleanly

--- explorer/utils/poll.py
+++ explorer/utils/poll.py
@@ -7,9 +7,11 @@
           https://www.boost.org/LICENSE_1_0.txt)
 """
 
-from typing import List, Callable, cast
+from typing import List, Callable, TypeVar, cast
+from contextlib import suppress
 import traceback
 
+from psycopg.errors import UniqueViolation
 from web3.types import LogReceipt
 from gevent import Greenlet
 from web3 import Web3
@@ -20,17 +22,29 @@ from explorer.utils.data import TOPICS, SYN_DATA
 # NOTE: :type:`EventData` is not really :type:`LogReceipt`,
 # but close enough to assume its type.
 CB = Callable[[str, str, LogReceipt], None]
+T = TypeVar('T')
+
+
+def retry(func: Callable[..., T], *args, **kwargs) -> T:
+    attempts = kwargs.pop('attempts', 3)
+
+    for _ in range(attempts):
+        try:
+            with suppress(*kwargs.pop('suppress', ())):
+                return func(*args, **kwargs)
+        except Exception:
+            print(f'retry attempt {_}, args: {args}')
+            traceback.print_exc()
+            gevent.sleep(2**_)
+
+    raise RuntimeError(f'maximum retries ({attempts}) reached')
 
 
 def log_loop(filter, chain: str, address: str, poll: int, cb: CB):
     while True:
         # `event` is of type `EventData`.
         for event in filter.get_new_entries():
-            try:
-                cb(chain, address, event)
-            except Exception:
-                print(chain, event)
-                traceback.print_exc()
+            retry(cb, chain, address, event, suppress=(UniqueViolation, ))
 
         gevent.sleep(poll)
 
